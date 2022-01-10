73a14031a94977a5a885edde877862531392ddc6
Megasatria <97154680+Megasatria@users.noreply.github.com>
2022-01-10 14:26:26 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,25 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Mesin Cetak Expired Date!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Mesin cetak Expired Date" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+<a href="https://megasatriahiciter.com">mesin cetak expired date</a>
+<a href="https://megasatriahiciter.com">alat cetak expiredate</a>
+<a href="https://megasatriahiciter.com">printer cetak expired date</a>
+<a href="https://megasatriahiciter.com">mesin inkjet coding</a>
+<a href="https://megasatriahiciter.com">mesin coding marking</a>
+<a href="https://megasatriahiciter.com">printer inkjet coding</a>
+<a href="https://megasatriahiciter.com">printer coding marking</a>
+<a href="https://megasatriahiciter.com">hitachi inkjet printer</a>
+<a href="https://megasatriahiciter.com">thermal inkjet printer</a>
+<a href="https://megasatriahiciter.com">expiry date machine</a>
+<a href="https://megasatriahiciter.com">inkjet coding machine</a>
+
         </p>
       </main>
 
