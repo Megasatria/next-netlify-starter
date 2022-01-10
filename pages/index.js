9ec0f8c66e771c6d5100e3d922ade28d95c25a9a
Megasatria <97154680+Megasatria@users.noreply.github.com>
2022-01-10 14:44:27 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,17 @@ export default function Home() {
       <main>
         <Header title="Mesin cetak Expired Date" />
         <p className="description">
-<a href="https://megasatriahiciter.com">mesin cetak expired date</a><br>
-<a href="https://megasatriahiciter.com">alat cetak expiredate</a><br>
-<a href="https://megasatriahiciter.com">printer cetak expired date</a><br>
-<a href="https://megasatriahiciter.com">mesin inkjet coding</a><br>
-<a href="https://megasatriahiciter.com">mesin coding marking</a><br>
-<a href="https://megasatriahiciter.com">printer inkjet coding</a><br>
-<a href="https://megasatriahiciter.com">printer coding marking</a><br>
-<a href="https://megasatriahiciter.com">hitachi inkjet printer</a><br>
-<a href="https://megasatriahiciter.com">thermal inkjet printer</a><br>
-<a href="https://megasatriahiciter.com">expiry date machine</a><br>
-<a href="https://megasatriahiciter.com">inkjet coding machine</a><br>
+<a href="https://megasatriahiciter.com">mesin cetak expired date</a></br>
+<a href="https://megasatriahiciter.com">alat cetak expiredate</a></br>
+<a href="https://megasatriahiciter.com">printer cetak expired date</a></br>
+<a href="https://megasatriahiciter.com">mesin inkjet coding</a></br>
+<a href="https://megasatriahiciter.com">mesin coding marking</a></br>
+<a href="https://megasatriahiciter.com">printer inkjet coding</a></br>
+<a href="https://megasatriahiciter.com">printer coding marking</a></br>
+<a href="https://megasatriahiciter.com">hitachi inkjet printer</a></br>
+<a href="https://megasatriahiciter.com">thermal inkjet printer</a></br>
+<a href="https://megasatriahiciter.com">expiry date machine</a></br>
+<a href="https://megasatriahiciter.com">inkjet coding machine</a></br>
 
         </p>
       </main>
